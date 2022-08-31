a1ee9f33b5c1b17b5f963e559aa57599c3b1c0d3
Gaurav <gouravj44@gmail.com>
2022-08-31 19:28:08 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,97 @@
+<!doctype php>
+<php lang="en">
+<head>
+<meta http-equiv="content-type" content="text/php; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Bisht Handloom</title>
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="vendors/font-awesome/css/fontawesome.css">
+<link rel="stylesheet" href="vendors/magnific-popup/magnific-popup.css">
+<link rel="stylesheet" href="vendors/slick/slick.css">
+<link rel="stylesheet" href="vendors/animate.css">
+
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id="site-wrapper" class="site-wrapper home-main">
+
+<header id="header" class="main-header header-sticky header-sticky-smart header-style-01 header-float bg-white text-uppercase">
+
+<div class="header-wrapper sticky-area">
+<div class="container container-1720">
+    
+<nav class="navbar navbar-expand-xl">
+<div class="header-mobile d-flex d-xl-none flex-fill justify-content-between align-items-center">
+<div class="navbar-toggler toggle-icon" data-toggle="collapse" data-target="#navbar-main-menu">
+<span></span>
+</div>
+<a class="navbar-brand navbar-brand-mobile" href="index.php">
+<img src="images/logo.png" alt="Bisht Handloom">
+</a>
+
+</div>
+<div class="collapse navbar-collapse" id="navbar-main-menu">
+<a class="navbar-brand d-none d-xl-block mr-auto x-animated x-fadeInUp" href="index.php" >
+<img src="images/logo.png" alt="Bisht handloom Home Furnishing shop">
+</a>
+
+<ul class="navbar-nav x-animated x-fadeInUp">
+<li class="nav-item">
+<a class="nav-link" href="index.php">Home</a>
+
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="home-furnishing-product.php">Furnishing
+<span class="caret"><i class="fas fa-angle-down"></i></span>
+</a>
+<div class="sub-menu x-menu-mega x-animated x-fadeInUp width-500">
+<div class="container">
+<div class="row w-100">
+<div class="col-lg-6">
+<ul class="mega-menu-col">
+<li class="nav-item"><a class="nav-link" href="bedsheets.php">
+Bedsheet</a></li>
+<li class="nav-item"><a class="nav-link" href="towels.php">
+Towels</a></li>
+<li class="nav-item"><a class="nav-link" href="cushion-covers.php">
+Cushion Covers</a></li>
+
+</ul>
+</div>
+ <div class="col-lg-6">
+<ul class="mega-menu-col">
+<li class="nav-item"><a class="nav-link" href="blankets.php">Blankets</a></li>
+<li class="nav-item"><a class="nav-link" href="table-covers.php">Table Covers</a></li>
+<li class="nav-item"><a class="nav-link" href="mats.php">
+Door Mats</a></li>
+
+</ul>
+
+</div>
+</div>
+</div>
+</div>
+</li>
+
+
+
+<li class="nav-item">
+<a class="nav-link" href="home-furnishing-product.php">Other products<span class="caret"></span></a>
+
+</li>
+
+<li class="nav-item">
+<a class="nav-link" href="contact.php">Contact Us <span class="caret"></span></a>
+ </li>
+<!-- <li class="nav-item">-->
+<!--<a class="nav-link" href="contact.php">About Us <span class="caret"></span></a>-->
+<!-- </li>-->
+ </ul>
+ 
+</div>
+</nav>
+</div>
+</div>
+</header>
